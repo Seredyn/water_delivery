1e327f72e9252820697a855b6be24708b450c569
admin <mr.seredyn@gmail.com>
2022-01-23 14:43:37 +0200

    Create model of Order. Before install LatLNG supports

---
+++ lib/models/order_model.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Order {
+  final String orderID;
+  final String orderClientID;
+  final Timestamp orderCreateTimeStamp;
+  final Map orderProductsList;
+  final String orderDeliveryAddress;
+  //final LatLng orderDeliveryAddress;
+  final String orderStatus;
+  final double orderPrise;
+  final String orderPayMethod;
+  final bool orderHasBeenPaid;
+  final Timestamp orderConfirmTimeStamp;
+  final Timestamp orderDeliveredTimeStamp;
+
+  Order({
+    required this.orderID,
+    required this.orderClientID,
+    required this.orderCreateTimeStamp,
+    required this.orderProductsList,
+    required this.orderDeliveryAddress,
+    required this.orderStatus,
+    required this.orderPrise,
+    required this.orderPayMethod,
+    required this.orderHasBeenPaid,
+    required this.orderConfirmTimeStamp,
+    required this.orderDeliveredTimeStamp,
+  });
+}
