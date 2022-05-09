9a695de05bb0621ae84d7ea903da46d88225ad2e
admin <mr.seredyn@gmail.com>
2022-05-09 19:08:20 +0300

    Добавил конвертер в модель юзера для получения екземпляра класса Юзера сразу из Файрбейс

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,8 +1,36 @@
-class User {
-  final String email = "";
-  final String phoneNumber = "";
-  final String name = "";
-  final String role = "";
-  final String userID = "";
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class WaterUser {
+  final String? email;
+  final String? phoneNumber;
+  final String? name;
+  final String? role;
+  final String? userID;
+
+  WaterUser({
+    this.email,
+    this.phoneNumber,
+    this.name,
+    this.role,
+    this.userID,
+  });
+
+  WaterUser.fromFirestore(
+      DocumentSnapshot<Map<String, dynamic>> snapshot,
+      SnapshotOptions? options,
+      )   : email = snapshot.data()?["email"],
+        phoneNumber = snapshot.data()?["phoneNumber"],
+        name = snapshot.data()?["name"],
+        role = snapshot.data()?["role"],
+        userID = snapshot.data()?["userID"];
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      if (email != null) "email": email,
+      if (phoneNumber != null) "phoneNumber": phoneNumber,
+      if (name != null) "name": name,
+      if (role != null) "role": role,
+      if (userID != null) "userID": userID,
+    };
+  }
 }
